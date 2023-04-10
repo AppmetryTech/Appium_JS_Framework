1728dd8d481c57e2ed358ae5f16d937b7488c7cf
MotghareChetan <chetan@appmetry.com>
2023-04-10 15:26:54 +0530

    Update TC_03_ValidateCheckOut.js

--- test/specs/TC_03_ValidateCheckOut.js
+++ test/specs/TC_03_ValidateCheckOut.js
@@ -4,17 +4,10 @@ const LoginPage = require('../pageobjects/login.page')
 const Catalog = require('../pageobjects/catalog.page')
 const Cart = require('../pageobjects/cart.page')
 const CheckOut = require('../pageobjects/checkout.page')
-const appium = require('appium');
-const { checkNodeOk } = require('appium/build/lib/config');
-const LoginData  = require('../../Data/login_data.json')
-const AddressData  = require('../../Data/address_data.json')
-const ProductData  = require('../../Data/product_detail.json')
-const PaymentData  = require('../../Data/payment_data.json')
-
-
-let cardNo = "3258 1265 7568 789"
-let expiryDate = "03/25"
-let cvv = "123"
+const LoginData = require('../../Data/login_data.json')
+const AddressData = require('../../Data/address_data.json')
+const ProductData = require('../../Data/product_detail.json')
+const PaymentData = require('../../Data/payment_data.json')
 
 describe('Validate CheckOut', () => {
     it('Validate Checkout', async () => {
@@ -30,7 +23,7 @@ describe('Validate CheckOut', () => {
         await CheckOut.entercountry(AddressData.country);
         await CheckOut.enterFullName(AddressData.fullName);
         await CheckOut.clickOnPayment();
-        await CheckOut.enterPaymentDetails(cardNo, expiryDate, cvv);
+        await CheckOut.enterPaymentDetails(PaymentData.cardNo, PaymentData.expiryDate, PaymentData.cvv);
         await CheckOut.clickreviewOrder();
         await CheckOut.clickplaceOrder();
     });
